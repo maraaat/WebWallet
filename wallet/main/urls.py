8480870a3d49b96ab9db5452d08402f5069a1037
Marat Akhmediev <akhmediev2003@mail.ru>
2024-05-10 00:31:43 +0500

    make an hierarchy"

--- wallet/main/urls.py
+++ wallet/main/urls.py
@@ -1,8 +1,8 @@
-
 from django.contrib import admin
 from django.urls import path, include
 
+from . import views
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include("main.urls"))
+    path('', views.start_window)
 ]
